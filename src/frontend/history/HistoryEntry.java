97a6db8de7ca7d1893c098d3b7fde0c663ec444f
Keping <kw238@duke.edu>
2017-02-27 01:22:19 -0500

    a prettier history view

--- src/frontend/history/HistoryEntry.java
+++ src/frontend/history/HistoryEntry.java
@@ -7,6 +7,7 @@ public class HistoryEntry extends Button {
 	
 	public HistoryEntry(FrontEndController frontEnd, String text) {
 		super(text);
+		this.setMaxWidth(10000); // hard coded.
 		this.getStyleClass().add("history-entry");
 		this.setOnAction(e -> frontEnd.appendText(this.getText()));
 	}
