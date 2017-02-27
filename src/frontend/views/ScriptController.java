e26614d56001351d0296908ba6e6be78dccc4073
Keping <kw238@duke.edu>
2017-02-26 19:00:54 -0500

    some view adjustments

--- src/frontend/views/ScriptController.java
+++ src/frontend/views/ScriptController.java
@@ -1,9 +1,14 @@
 package frontend.views;
 
 import frontend.app.FrontEndController;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
 public class ScriptController {
 
+	@FXML
+	private TextArea scriptArea;
+	
 	private FrontEndController frontEnd;
 	public void setFrontEndController(FrontEndController frontEnd) {
 		this.frontEnd = frontEnd;
