db47821205e7ef74605c2a256c1f59abe5b7f4f2
Matthew Tribby <mrt28@duke.edu>
2017-02-27 02:41:42 -0500

    Removed clutter

--- src/frontend/app/Main.java
+++ src/frontend/app/Main.java
@@ -1,6 +1,5 @@
 package frontend.app;
 
-import java.awt.Toolkit;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
@@ -27,7 +26,6 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(MAIN_PAGE_RESOURCE));
         loader.setResources(getResourceBundle());
 
-        
         AnchorPane root = (AnchorPane) loader.load();
         scene = new Scene(root, WIDTH, HEIGHT);
         
---
+++ src/frontend/history/HistoryEntry.java
@@ -0,0 +1,15 @@
+package frontend.history;
+
+import frontend.app.FrontEndController;
+import javafx.scene.control.Button;
+
+public class HistoryEntry extends Button {
+	
+	public HistoryEntry(FrontEndController frontEnd, String text) {
+		super(text);
+		this.setMaxWidth(10000); // hard coded.
+		this.getStyleClass().add("history-entry");
+		this.setOnAction(e -> frontEnd.appendText(this.getText()));
+	}
+
+}
