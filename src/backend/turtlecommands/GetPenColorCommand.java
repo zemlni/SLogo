1a0bf868451e027cb82feeee9fe51e79a501b8bd
Nikita Zemlevskiy <naz7@duke.edu>
2017-03-10 01:24:49 -0500

    add comments

--- src/backend/turtlecommands/GetPenColorCommand.java
+++ src/backend/turtlecommands/GetPenColorCommand.java
@@ -4,13 +4,22 @@ import backend.BackendController;
 import backend.Command;
 import backend.parser.Input;
 
+/**
+ * @author nikita get the index of the current pen color
+ */
 public class GetPenColorCommand extends Command {
 
 	public GetPenColorCommand(Input info, BackendController controller) {
 		super(info, controller, 0);
 	}
+
+	/**
+	 * get the current pen color
+	 * 
+	 * @return the index of the current pen color
+	 */
 	@Override
-	public double execute(){
+	public double execute() {
 		return getBackendController().getFrontEndController().getDisplayController().getPenColor();
 	}
 }
